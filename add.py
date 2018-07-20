8d967659339b575283dbc547a984584de1017b7a
Will Smith <lwilsonsmith@gmail.com>
2018-07-20 18:18:16 -0400

    Think this was the issue

--- add.py
+++ add.py
@@ -4,20 +4,19 @@ reload(sys)
 from pypardot.client import PardotAPI
 
 visitor_id = sys.argv[1]
-print(visitor_id)
-# email = sys.argv[2]
-# password = sys.argv[3]
-# key = sys.argv[4]
-# list_id = '98751'
-# api_version = 3
-# p = PardotAPI(
-#   email=email,
-#   password=password,
-#   user_key=key,
-#   version=api_version
-#   )
-# p.authenticate()
-# visitor = p.visitors.read(visitor_id)['visitor']
-# prospect_id = visitor['prospect']['id']
-# response = p.listmemberships.create(list_id, prospect_id)
-# print(response['@attributes']['stat'])
+email = sys.argv[2]
+password = sys.argv[3]
+key = sys.argv[4]
+list_id = '98751'
+api_version = 3
+p = PardotAPI(
+  email=email,
+  password=password,
+  user_key=key,
+  version=api_version
+  )
+p.authenticate()
+visitor = p.visitors.read(visitor_id)['visitor']
+prospect_id = visitor['prospect']['id']
+response = p.listmemberships.create(list_id, prospect_id)
+print(response['@attributes']['stat'])
