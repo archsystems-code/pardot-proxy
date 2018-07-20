336b231b37c8345d161ed9fb6dc42af871c4420f
Will Smith <lwilsonsmith@gmail.com>
2018-07-20 16:53:53 -0400

    Get everything working

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express');
 const PythonShell = require('python-shell');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 const key = process.env.KEY;
 
-const addToList = async id => {
-  return await PythonShell.run('app.py', {mode: 'text', args: [id, email, password, key]},  (err, results) => results)
-}
-
 app.use(express.json());
+app.use(cors())
 app.post('/add-to-cart-list', async (req, res) => {
-  const visitor = req.query.visitor;
-  const results = await addToList(visitor)
-  console.log(results)
-  res.send({status: results})
+  const visitor = req.body.visitor
+  PythonShell.run('app.py', { mode: 'text', args: [visitor, email, password, key], pythonPath: 'python3' }, (err, results) => {
+    if (err) throw err;
+    res.send({ status: results[0] })
+  })
 })
 app.listen(port, () => console.log(`Listening on port ${port}`));
